c8a895284d3043332471c7aeadb3969f3e0ef338
Anas Ahmad <anas.ahmad@devsinc.com>
2021-12-22 21:11:07 +0500

    first commit

---
+++ src/app/Directives/renderer-highlight.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appRendererHighlight]'
+})
+export class RendererHighlightDirective {
+
+  @HostBinding('style.backgroundColor') backgroundColor:string = 'transparent'
+  @HostBinding('style.color') color:string = ''
+  @HostBinding('style.border') border:string = '2px solid rgba(96, 102, 208, 0.7)'
+
+  constructor(private element:ElementRef, private renderer: Renderer2) { }
+
+  @HostListener('mouseenter') mouseover(eventData: Event){
+    this.backgroundColor = '#F9A826'
+    this.color = 'white'
+    this.border = '2px solid transparent'
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.backgroundColor = 'transparent'
+    this.color = '#6066D0'
+    this.border = '2px solid rgba(96, 102, 208, 0.7)'
+  }
+}
